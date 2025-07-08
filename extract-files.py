96c5871dc38339c67fcd99866b4cc867451e474c
Omkar Chandorkar <gotenksIN@aospa.co>
2025-07-08 11:53:28 -0400

    sm8650-common: patch libwfdservice to use the correct android.hardware.common

--- extract-files.py
+++ extract-files.py
@@ -73,6 +73,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .add_needed('libaudioclient_shim.sm8650.so')
+        .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/lib64/libcamximageformatutils.so': blob_fixup()
         .remove_needed('android.hardware.graphics.allocator-V1-ndk.so'),
