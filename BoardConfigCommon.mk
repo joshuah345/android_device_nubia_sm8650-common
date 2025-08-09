9e6ceaee75622ac848a34cc308e865d4161887de
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    sm8650-common: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,9 +64,8 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 SOONG_CONFIG_qtidisplay_udfps := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
     $(COMMON_PATH)/device_framework_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := \
