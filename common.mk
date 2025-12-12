977e5e52474026006b6289ae0951438a92e8e9dc
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-12 14:47:58 -0500

    sm8650-common: Update some soong config variables to bool type

--- common.mk
+++ common.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
-$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # Display
 PRODUCT_PACKAGES += \
@@ -392,7 +392,7 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.nubia_sm8650-richtap)
-$(call soong_config_set,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
 
 # WiFi
 PRODUCT_PACKAGES += \
