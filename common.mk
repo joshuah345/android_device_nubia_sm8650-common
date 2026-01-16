7911e6add5378bfd2b8a50aa84abf133b20d4fb3
hiroshi <hiroshi@tanuki.page>
2026-01-16 12:16:58 -0500

    sm8650-common: overlay: Rename WallpaperPicker2 overlays One or more roms may have merged a variant of these, we want to use our own

--- common.mk
+++ common.mk
@@ -223,8 +223,8 @@ PRODUCT_PACKAGES += \
     NcmTetheringOverlay \
     SystemUIResCommon_Sys \
     TelephonyResCommon_Sys \
-    WallpaperPicker2Overlay \
-    WallpaperPicker2PixelOverlay \
+    NubiaWallpaperPicker2Overlay \
+    NubiaWallpaperPicker2PixelOverlay \
     WifiResCommon_Sys \
     WifiResOverlay \
     WifiResTarget \
