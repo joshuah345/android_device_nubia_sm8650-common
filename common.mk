3da0a065ca369646f49382a4c0528e2f9388d4de
hiroshi <hiroshi@tanuki.page>
2026-01-23 02:24:53 -0500

    sm8650-common: Drop NubiaGameSwitch

--- common.mk
+++ common.mk
@@ -420,7 +420,6 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, packages/apps/NubiaParts/config.mk)
 
 PRODUCT_PACKAGES += \
-    NubiaGameSwitch \
     NubiaPowerUtils
 
 # Inherit from proprietary targets
