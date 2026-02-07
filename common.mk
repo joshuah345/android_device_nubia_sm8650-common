26260e3ea006070d6c1f27e27da5289a3efc2886
hiroshi <hiroshi@tanuki.page>
2026-02-07 16:47:35 -0500

    Revert "sm8650-common: Drop NubiaGameSwitch"
    This reverts commit 5e7ea86044c643857b5112cecfe4bfe6e51741e8.

--- common.mk
+++ common.mk
@@ -420,6 +420,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, packages/apps/NubiaParts/config.mk)
 
 PRODUCT_PACKAGES += \
+    NubiaGameSwitch \
     NubiaPowerUtils
 
 # Inherit from proprietary targets
